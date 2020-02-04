91fafe94e8aceea583abda74a8b44ed1848b9d45
Jeroen van Riel <jeroenvanriel@outlook.com>
2020-02-04 15:20:58 +0100

    implemented an action queue for movement

--- serpentine/move_agent.py
+++ serpentine/move_agent.py
@@ -1,3 +1,6 @@
+import numpy as np
+import queue
+
 from pommerman import characters
 from pommerman.constants import Action
 from pommerman.agents import BaseAgent
@@ -9,6 +12,29 @@ class MoveAgent(BaseAgent):
     def __init__(self, character=characters.Bomber):
         super().__init__(character)
 
+        # this queue will hold the actions for future turns
+        self._actionQueue = queue.SimpleQueue()
+
     def act(self, obs, action_space):
-        # Main event that is being called on every turn.
-        return Action.Stop
+        my_position = tuple(obs['position'])
+
+        # the position that we want to reach
+        goal = (5, 5)
+
+        # determine if we must go left or right
+        if my_position[0] < goal[0]:
+            # we must go to the right
+            self._actionQueue.put(Action.Right)
+        elif my_position[0] > goal[0]:
+            # we must go to the left
+            self._actionQueue.put(Action.Left)
+
+        # determine if we must go up or down
+        if my_position[1] < goal[1]:
+            # we are above the goal
+            self._actionQueue.put(Action.Down)
+        elif my_position[1] > goal[1]:
+            # we are under the goal
+            self._actionQueue.put(Action.Up)
+
+        return self._actionQueue.get()
